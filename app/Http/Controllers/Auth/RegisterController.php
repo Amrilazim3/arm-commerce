44e7d036e62bd1e2e5a595a9390a40ac45d1fd33
Amrilazim3 <a.azim0711@gmail.com>
2022-04-20 15:41:14 +0800

    create register controller for authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
@@ -13,13 +15,43 @@ class RegisterController extends Controller
         return Inertia::render('Auth/Register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        // register user credentials in database
+        $validated = $request->validate([
+            'name' => ['required', 'max:50'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:7', 'confirmed'],
+        ]);
+
+        if ($validated) {
+            User::create($validated);
+
+            if (Auth::attempt(
+                [
+                    'email' => $request->email,
+                    'password' => $request->password
+                ],
+                $request->isRememberMe == 1 ? true : false
+            )) {
+                $request->session()->regenerate();
+
+                return redirect()->intended('/products');
+            }
+
+            return false;
+        }
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
-        // destroy the user account
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        User::where('id', $request->user()->id)->delete();
+
+        return redirect('/');
     }
 }
