a94d165c71cb6e38c2c363aafdbfced46512816e
Amrilazim3 <a.azim0711@gmail.com>
2022-04-21 17:56:18 +0800

    update route changes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+
+Route::get('/products', [ProductController::class, 'index']);
 
 Route::middleware('guest')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Home');
-    });
+    Route::get('/', [HomeController::class, 'index']);
     
-    Route::get('/login', function (Request $request) {
-        return Inertia::render('Auth/Login');
-    });
-    Route::get('/register', function (Request $request) {
-        return Inertia::render('Auth/Register');
-    });
+    Route::get('/register', [RegisterController::class, 'index']);
+    Route::post('/register', [RegisterController::class, 'store']);
+    Route::get('/login', [LoginController::class, 'index']);
+    Route::post('/login', [LoginController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'destroy']);
 });
 
