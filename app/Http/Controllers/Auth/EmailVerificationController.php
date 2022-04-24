cd2e023d5496fbccc7c1bdf7c40be57ed3026fee
Amrilazim3 <a.azim0711@gmail.com>
2022-04-24 09:44:31 +0800

    make controller for email verification

---
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class EmailVerificationController extends Controller
+{
+    public function index(Request $request): RedirectResponse
+    {
+        $user = User::find($request->route('id'));
+
+        if ($user->hasVerifiedEmail()) {
+            return redirect(config('app.url') . '/products')->with('success', 'email already verified');
+        }
+
+        if ($user->markEmailAsVerified()) {
+            event(new Verified($user));
+        }
+
+        return redirect(config('app.url') . '/products')->with('success', 'email is verified');
+    }
+
+    public function resend(Request $request)
+    {
+        $request->user()->sendEmailVerificationNotification();
+        return redirect('/products');
+    }
+}
