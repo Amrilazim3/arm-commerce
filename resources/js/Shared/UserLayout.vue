96ce07f945f0c9177d5b5021a6fb203631d98350
Amrilazim3 <a.azim0711@gmail.com>
2022-04-26 11:52:17 +0800

    Layout for default user

---
+++ resources/js/Shared/UserLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <Header />
+    </section>
+
+    <section>
+        <AlertMessage />
+        <VerificationNotice />
+        <slot />
+    </section>
+
+    <section>
+        <Footer />
+    </section>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import VerificationNotice from "./VerificationNotice.vue";
+import AlertMessage from "./AlertMessage.vue";
+
+export default {
+    components: { Header, Footer, VerificationNotice, AlertMessage },
+};
+</script>
