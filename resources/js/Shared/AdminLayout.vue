bdd5b31792d4f4d6cd703acb347d16e7fc4fecb4
Amrilazim3 <a.azim0711@gmail.com>
2022-04-26 11:52:48 +0800

    layout for admin

---
+++ resources/js/Shared/AdminLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <MainNav />
+    </section>
+
+    <section>
+        <AlertMessage />
+        <slot />
+    </section>
+
+    <section>
+        <Footer />
+    </section>
+</template>
+
+<script>
+import MainNav from "./MainNav.vue";
+import Footer from "./Footer.vue";
+import AlertMessage from "./AlertMessage.vue";
+
+export default {
+    components: {
+        MainNav,
+        Footer,
+        AlertMessage
+    },
+};
+</script>
