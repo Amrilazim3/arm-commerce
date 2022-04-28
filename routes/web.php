dd0d815b2085c789867b46a76ee1f1f69ea5ebc6
Amrilazim3 <a.azim0711@gmail.com>
2022-04-28 12:26:39 +0800

    adding name to every resource controller

--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,24 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\User\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
 Route::middleware('guest')->group(function () {
-    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
     
-    Route::get('/register', [RegisterController::class, 'index']);
-    Route::post('/register', [RegisterController::class, 'store']);
-    Route::get('/login', [LoginController::class, 'index']);
-    Route::post('/login', [LoginController::class, 'store']);
+    Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+    Route::post('/login', [LoginController::class, 'store'])->name('login.store');
     Route::get('/oauth/{service}', [OAuthServiceController::class, 'redirect']);
     Route::get('/oauth/{service}/callback', [OAuthServiceController::class, 'handleCallback']);
     Route::post('/oauth/login', [OAuthServiceController::class, 'login']);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::post('/logout', [LoginController::class, 'destroy']);
+    Route::post('/logout', [LoginController::class, 'destroy'])->name('login.destroy');
 
-    Route::get('/user/account/profile', [ProfileController::class, 'index']);
+    Route::get('/user/account/profile', [ProfileController::class, 'index'])->name('profile.index');
 });
 
 Route::prefix('/email/verify')->name('verification.')->group(function () {
