70235e32ada94a754619c711703cef5e457cc7c6
Amrilazim3 <a.azim0711@gmail.com>
2022-05-04 23:31:18 +0800

    commit bug changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="dumbbell-solid.svg">
+    <link rel="icon" href="https://arm-commerce.com/dumbbell-solid.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
