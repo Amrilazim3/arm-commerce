89103af31a88eb7dc74425d5f2fc1f7c631698e7
Amrilazim3 <a.azim0711@gmail.com>
2022-05-11 15:02:20 +0800

    create controller for changing email

---
+++ app/Http/Controllers/User/Account/ChangeEmailController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User\Account;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ChangeEmailController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('User/Account/ChangeEmail');
+    }
+}
