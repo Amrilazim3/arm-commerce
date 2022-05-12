d03ac0f9e27ad0b74acd8046621c51ee038162c7
Amrilazim3 <a.azim0711@gmail.com>
2022-05-12 18:45:49 +0800

    commit changes

--- public/js/resources_js_Pages_Home_vue.js
+++ public/js/resources_js_Pages_Home_vue.js
@@ -41,18 +41,37 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var _hoisted_1 = {
+
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("title", null, "Home", -1
+/* HOISTED */
+);
+
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("meta", {
+  name: "description",
+  content: "explore our best products for healthy and strong people."
+}, null, -1
+/* HOISTED */
+);
+
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("meta", {
+  name: "keywords",
+  content: "arm-commerce"
+}, null, -1
+/* HOISTED */
+);
+
+var _hoisted_4 = {
   style: {
     "background-image": "url('images/gym-background.jpg')",
     "background-repeat": "no-repeat",
     "background-size": "cover"
   }
 };
-var _hoisted_2 = {
+var _hoisted_5 = {
   "class": "flex items-center justify-center min-h-screen px-4 mx-auto text-center text-white xl:max-w-5xl lg:max-w-4xl sm:px-6 md:px-8"
 };
 
-var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", {
+var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", {
   "class": "text-5xl font-bold"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" Commerce For "), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", {
   "class": "text-orange-400"
@@ -62,37 +81,42 @@ var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
 /* HOISTED */
 );
 
-var _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
   "class": "mt-6 text-lg font-medium"
 }, " We are selling only the best product from gym,fitness equipment to supplement for body builder, sports people or who wants to lost fat. All our products came from a trusted supplier from U.K ", -1
 /* HOISTED */
 );
 
-var _hoisted_5 = {
+var _hoisted_8 = {
   "class": "hover:scale-125 transition ease-out delay-75 duration-300 items-center justify-center px-4 py-2 mt-4 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border hover:from-purple-700 hover:to-indigo-700"
 };
 
-var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" View Products Now ");
+var _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" View Products Now ");
 
-var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"max-w-2xl px-8 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8\"><h2 class=\"text-2xl font-extrabold tracking-tight text-gray-900\"> Main Category </h2><div class=\"grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8\"><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_gym_equipment_jpg__WEBPACK_IMPORTED_MODULE_1__["default"] + "\" alt=\"Gym equipment pictures\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">Gym Equipment</h1></div></div><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_fitness_equipment_jpg__WEBPACK_IMPORTED_MODULE_2__["default"] + "\" alt=\"Fitness equipment pictures\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">Fitness Equipment</h1></div></div><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_supplement_jpg__WEBPACK_IMPORTED_MODULE_3__["default"] + "\" alt=\"Supplement pictures\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">Supplement</h1></div></div><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_more_jpg__WEBPACK_IMPORTED_MODULE_4__["default"] + "\" alt=\"more variations\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">More</h1></div></div></div></div><div class=\"relative py-16 bg-gray-50 sm:py-24 lg:py-32\"><div class=\"relative\"><div class=\"max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl\"><h2 class=\"text-base font-semibold tracking-wider uppercase text-cyan-600\"> New </h2><p class=\"mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl\"> Buy with bundle!!! </p><p class=\"mx-auto mt-5 text-xl text-gray-500 max-w-prose\"> Buy all equipment all in one and get the best deals to save up your pocket. </p></div><div class=\"grid max-w-md gap-8 px-4 mx-auto mt-12 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl\"><div class=\"flex flex-col overflow-hidden rounded-lg shadow-lg\"><div class=\"flex-shrink-0\"><img class=\"object-cover w-full h-48\" src=\"" + _assets_gym_bundle_package_jpg__WEBPACK_IMPORTED_MODULE_5__["default"] + "\" alt=\"bundle-package-one\"></div><div class=\"flex flex-col justify-between flex-1 p-6 bg-white\"><div class=\"flex-1\"><p class=\"block mt-2 text-xl font-semibold text-gray-900\"> Gym bundle starter </p><p class=\"mt-3 text-base text-gray-500\"> Fot the people who want to workout at home and doesn&#39;t have much time to go to the gym. </p></div></div></div><div class=\"flex flex-col overflow-hidden rounded-lg shadow-lg\"><div class=\"flex-shrink-0\"><img class=\"object-cover w-full h-48\" src=\"" + _assets_fitness_bundle_package_jpg__WEBPACK_IMPORTED_MODULE_6__["default"] + "\" alt=\"fitness-bundle-package\"></div><div class=\"flex flex-col justify-between flex-1 p-6 bg-white\"><div class=\"flex-1\"><p class=\"block mt-2 text-xl font-semibold text-gray-900\"> Fitness-For-Life Bundle </p><p class=\"mt-3 text-base text-gray-500\"> Suitable for fat burn, light wight workout and to get those numb mussle running up. </p></div></div></div><div class=\"flex flex-col overflow-hidden rounded-lg shadow-lg\"><div class=\"flex-shrink-0\"><img class=\"object-cover w-full h-48\" src=\"" + _assets_supplement_bundle_package_jpg__WEBPACK_IMPORTED_MODULE_7__["default"] + "\" alt=\"\"></div><div class=\"flex flex-col justify-between flex-1 p-6 bg-white\"><div class=\"flex-1\"><p class=\"block mt-2 text-xl font-semibold text-gray-900\"> Shreded starter package </p><p class=\"mt-3 text-base text-gray-500\"> For those who want to build mussle and burn some fat, This bundle package is perfect for you cause it come with 3 best pre workout and post workout supplement. </p></div></div></div></div></div></div>", 2);
+var _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"max-w-2xl px-8 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8\"><h2 class=\"text-2xl font-extrabold tracking-tight text-gray-900\"> Main Category </h2><div class=\"grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8\"><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_gym_equipment_jpg__WEBPACK_IMPORTED_MODULE_1__["default"] + "\" alt=\"Gym equipment pictures\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">Gym Equipment</h1></div></div><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_fitness_equipment_jpg__WEBPACK_IMPORTED_MODULE_2__["default"] + "\" alt=\"Fitness equipment pictures\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">Fitness Equipment</h1></div></div><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_supplement_jpg__WEBPACK_IMPORTED_MODULE_3__["default"] + "\" alt=\"Supplement pictures\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">Supplement</h1></div></div><div class=\"relative group\"><div class=\"w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none\"><img src=\"" + _assets_more_jpg__WEBPACK_IMPORTED_MODULE_4__["default"] + "\" alt=\"more variations\" class=\"object-cover object-center w-full h-full lg:w-full lg:h-full\"></div><div class=\"mt-4\"><h1 class=\"text-lg font-bold\">More</h1></div></div></div></div><div class=\"relative py-16 bg-gray-50 sm:py-24 lg:py-32\"><div class=\"relative\"><div class=\"max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl\"><h2 class=\"text-base font-semibold tracking-wider uppercase text-cyan-600\"> New </h2><p class=\"mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl\"> Buy with bundle!!! </p><p class=\"mx-auto mt-5 text-xl text-gray-500 max-w-prose\"> Buy all equipment all in one and get the best deals to save up your pocket. </p></div><div class=\"grid max-w-md gap-8 px-4 mx-auto mt-12 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl\"><div class=\"flex flex-col overflow-hidden rounded-lg shadow-lg\"><div class=\"flex-shrink-0\"><img class=\"object-cover w-full h-48\" src=\"" + _assets_gym_bundle_package_jpg__WEBPACK_IMPORTED_MODULE_5__["default"] + "\" alt=\"bundle-package-one\"></div><div class=\"flex flex-col justify-between flex-1 p-6 bg-white\"><div class=\"flex-1\"><p class=\"block mt-2 text-xl font-semibold text-gray-900\"> Gym bundle starter </p><p class=\"mt-3 text-base text-gray-500\"> Fot the people who want to workout at home and doesn&#39;t have much time to go to the gym. </p></div></div></div><div class=\"flex flex-col overflow-hidden rounded-lg shadow-lg\"><div class=\"flex-shrink-0\"><img class=\"object-cover w-full h-48\" src=\"" + _assets_fitness_bundle_package_jpg__WEBPACK_IMPORTED_MODULE_6__["default"] + "\" alt=\"fitness-bundle-package\"></div><div class=\"flex flex-col justify-between flex-1 p-6 bg-white\"><div class=\"flex-1\"><p class=\"block mt-2 text-xl font-semibold text-gray-900\"> Fitness-For-Life Bundle </p><p class=\"mt-3 text-base text-gray-500\"> Suitable for fat burn, light wight workout and to get those numb mussle running up. </p></div></div></div><div class=\"flex flex-col overflow-hidden rounded-lg shadow-lg\"><div class=\"flex-shrink-0\"><img class=\"object-cover w-full h-48\" src=\"" + _assets_supplement_bundle_package_jpg__WEBPACK_IMPORTED_MODULE_7__["default"] + "\" alt=\"\"></div><div class=\"flex flex-col justify-between flex-1 p-6 bg-white\"><div class=\"flex-1\"><p class=\"block mt-2 text-xl font-semibold text-gray-900\"> Shreded starter package </p><p class=\"mt-3 text-base text-gray-500\"> For those who want to build mussle and burn some fat, This bundle package is perfect for you cause it come with 3 best pre workout and post workout supplement. </p></div></div></div></div></div></div>", 2);
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_Head = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Head");
 
   var _component_Link = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Link");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Head, {
-    title: "Home"
-  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("main", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_3, _hoisted_4, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Link, {
-    href: "/products"
-  }, {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Head, null, {
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [_hoisted_6];
+      return [_hoisted_1, _hoisted_2, _hoisted_3];
     }),
     _: 1
     /* STABLE */
 
-  })])])])]), _hoisted_7])], 64
+  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("main", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [_hoisted_6, _hoisted_7, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Link, {
+    href: "/products"
+  }, {
+    "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+      return [_hoisted_9];
+    }),
+    _: 1
+    /* STABLE */
+
+  })])])])]), _hoisted_10])], 64
   /* STABLE_FRAGMENT */
   );
 }
