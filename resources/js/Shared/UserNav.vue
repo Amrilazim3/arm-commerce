9f2ac96e731e10624ab5c1e45405bfa4b3d9874c
Amrilazim3 <a.azim0711@gmail.com>
2022-05-13 07:54:15 +0800

    creating change password pages

--- resources/js/Shared/UserNav.vue
+++ resources/js/Shared/UserNav.vue
@@ -69,10 +69,10 @@
 
                     <MenuItem>
                         <Link
-                            href="/user/account/password"
+                            href="/user/account/change-password"
                             :class="[
                                 'group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-violet-500 hover:text-gray-100 text-gray-900',
-                                $page.component == 'User/Account/Password'
+                                $page.component == 'User/Account/ChangePassword'
                                     ? 'bg-violet-500 text-white'
                                     : '',
                             ]"
@@ -245,10 +245,10 @@
                     </li>
                     <li>
                         <Link
-                            href="/user/account/password"
+                            href="/user/account/change-password"
                             class="hover:underline"
                             :class="
-                                $page.component == 'User/Account/Password'
+                                $page.component == 'User/Account/ChangePassword'
                                     ? 'underline'
                                     : ''
                             "
