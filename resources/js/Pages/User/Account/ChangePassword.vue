0950c95c10ce4cd34d21db164f060c5be34e27e6
Amrilazim3 <a.azim0711@gmail.com>
2022-05-14 04:53:49 +0800

    commit changes

--- resources/js/Pages/User/Account/ChangePassword.vue
+++ resources/js/Pages/User/Account/ChangePassword.vue
@@ -1,11 +1,161 @@
 <template>
-    <div>
-        change password page
+    <Head title="Change Password" />
+    <div class="lg:flex">
+        <UserNav />
+        <form
+            class="px-10 lg:pl-10 lg:pr-28 py-6 space-y-8 divide-y divide-gray-200 lg:flex-1"
+            @submit.prevent="updateProfile"
+        >
+            <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+                <div>
+                    <div>
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            Change Password
+                        </h3>
+                    </div>
+
+                    <div class="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+                        >
+                            <label
+                                for="old-password"
+                                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                            >
+                                Old password
+                            </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <div class="max-w-lg flex rounded-md shadow-sm">
+                                    <input
+                                        type="password"
+                                        name="old-password"
+                                        id="old-password"
+                                        autocomplete="password"
+                                        class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                                        placeholder="Enter your name"
+                                        v-model="form.oldPassword"
+                                        required
+                                    />
+                                </div>
+                                <div
+                                    v-if="form.errors.oldPassword"
+                                    class="text-red-500 text-sm mt-1"
+                                >
+                                    {{ form.errors.oldPassword }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+                        >
+                            <label
+                                for="new-password"
+                                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                            >
+                                New password
+                            </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <div class="max-w-lg flex rounded-md shadow-sm">
+                                    <input
+                                        type="password"
+                                        name="new-password"
+                                        id="new-password"
+                                        class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                                        placeholder="Enter your name"
+                                        v-model="form.newPassword"
+                                        required
+                                    />
+                                </div>
+                                <div
+                                    v-if="form.errors.newPassword"
+                                    class="text-red-500 text-sm mt-1"
+                                >
+                                    {{ form.errors.newPassword }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+                        >
+                            <label
+                                for="new-password-confirmation"
+                                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                            >
+                                New password confirmation
+                            </label>
+                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                <div class="max-w-lg flex rounded-md shadow-sm">
+                                    <input
+                                        type="password"
+                                        name="new-password-confirmation"
+                                        id="new-password-confirmation"
+                                        class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                                        placeholder="Enter your name"
+                                        v-model="form.newPasswordConfirmation"
+                                        required
+                                    />
+                                </div>
+                                <div
+                                    v-if="form.errors.newPasswordConfirmation"
+                                    class="text-red-500 text-sm mt-1"
+                                >
+                                    {{ form.errors.newPasswordConfirmation }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div
+                            class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+                        >
+                            <Link
+                                href="/user/account/forgot-password"
+                                class="text-sm font-bold text-blue-500"
+                            >
+                                Forgot password
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pt-5">
+                <div class="flex justify-end">
+                    <button
+                        type="submit"
+                        class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white"
+                        :disabled="form.processing || !form.isDirty"
+                        :class="
+                            form.processing || !form.isDirty
+                                ? 'cursor-not-allowed bg-indigo-300'
+                                : 'bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                        "
+                    >
+                        Change
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+import UserNav from "../../../Shared/UserNav.vue";
+
 export default {
-    
-}
+    components: {
+        UserNav,
+    },
+
+    data() {
+        return {
+            form: this.$inertia.form({
+                oldPassword: "",
+                newPassword: "",
+                newPasswordConfirmation: "",
+            }),
+        };
+    },
+};
 </script>
