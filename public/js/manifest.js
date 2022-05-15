7f15e91e58aba4ab3cb0b0b149d5763d76619758
Amrilazim3 <a.azim0711@gmail.com>
2022-05-15 16:04:25 +0800

    commit changes

--- public/js/manifest.js
+++ public/js/manifest.js
@@ -137,7 +137,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames not based on template
-/******/ 			if ({"resources_js_Pages_Auth_Login_vue":1,"resources_js_Pages_Auth_Register_vue":1,"resources_js_Pages_Home_vue":1,"resources_js_Pages_Products_vue":1,"resources_js_Pages_User_Account_ChangeEmail_vue":1,"resources_js_Pages_User_Account_ChangePassword_vue":1,"resources_js_Pages_User_Account_Profile_vue":1}[chunkId]) return "js/" + chunkId + ".js";
+/******/ 			if ({"resources_js_Pages_Auth_Login_vue":1,"resources_js_Pages_Auth_Register_vue":1,"resources_js_Pages_Home_vue":1,"resources_js_Pages_Products_vue":1,"resources_js_Pages_User_Account_ChangeEmail_vue":1,"resources_js_Pages_User_Account_ChangePassword_vue":1,"resources_js_Pages_User_Account_ForgotPassword_vue":1,"resources_js_Pages_User_Account_Profile_vue":1,"resources_js_Pages_User_Account_ResetPassword_vue":1}[chunkId]) return "js/" + chunkId + ".js";
 /******/ 			// return url for filenames based on template
 /******/ 			return undefined;
 /******/ 		};
