d563e07dce0779942918e5b09260cdb8fe60c1b3
Amrilazim3 <a.azim0711@gmail.com>
2022-05-15 18:06:09 +0800

    redirect back user without flash any session if reset link successfully sent

--- app/Http/Controllers/User/Account/ForgotPasswordController.php
+++ app/Http/Controllers/User/Account/ForgotPasswordController.php
@@ -25,7 +25,7 @@ class ForgotPasswordController extends Controller
         );
      
         return $status === Password::RESET_LINK_SENT
-                    ? back()->with(['status' => __($status)])
+                    ? back()
                     : back()->withErrors(['email' => __($status)]);
     }
 }
