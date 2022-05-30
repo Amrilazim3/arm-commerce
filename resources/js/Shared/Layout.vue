98fafe37bda9b447a1f9a28d3149ce33b5b86e80
Amrilazim3 <a.azim0711@gmail.com>
2022-05-30 23:19:13 +0800

    rename file

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <Header />
+    </section>
+
+    <section>
+        <Notifications />
+        <VerificationNotice />
+        <slot />
+    </section>
+
+    <section>
+        <Footer />
+    </section>
+</template>
+
+<script>
+import Header from "./Partials/Header.vue";
+import Footer from "./Partials/Footer.vue";
+import VerificationNotice from "./VerificationNotice.vue";
+import Notifications from "./Notifications.vue";
+
+export default {
+    components: { Header, Footer, VerificationNotice, Notifications },
+};
+</script>
