f9e5fdd04e1a21eaae25ef84a93c43a764e31778
Amrilazim3 <a.azim0711@gmail.com>
2022-05-30 23:18:54 +0800

    rename file

---
+++ resources/js/Shared/HideSubNavLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <MainNav />
+    </section>
+
+    <section>
+        <Notifications />
+        <slot />
+    </section>
+
+    <section>
+        <Footer />
+    </section>
+</template>
+
+<script>
+import MainNav from "./Partials/MainNav.vue";
+import Footer from "./Partials/Footer.vue";
+import Notifications from "./Notifications.vue";
+
+export default {
+    components: {
+        MainNav,
+        Footer,
+        Notifications
+    },
+};
+</script>
