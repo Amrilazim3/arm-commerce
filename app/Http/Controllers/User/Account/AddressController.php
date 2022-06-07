dc7a46b813140b9eea079afd7c2eecab89885b55
Amrilazim3 <a.azim0711@gmail.com>
2022-06-07 17:50:30 +0800

    storing address data to database

--- app/Http/Controllers/User/Account/AddressController.php
+++ app/Http/Controllers/User/Account/AddressController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\User\Account;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
+use App\Rules\PhoneNumberValidation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AddressController extends Controller
@@ -13,14 +16,28 @@ class AddressController extends Controller
         return Inertia::render('User/Account/Addresses');
     }
 
-    public function create()
-    {
-        return Inertia::render('User/Account/CreateAddress');
-    }
-
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'phoneNumber' => ['required', new PhoneNumberValidation],
+            'fullName' => ['required'],
+            'state' => ['required'],
+            'city' => ['required'],
+            'postalCode' => ['required', 'integer', 'digits:5', 'postal_code:MY'],
+            'streetName' => ['required']
+        ]);
+
+        Address::create([
+            'user_id' => Auth::user()->id,
+            'full_name' => $request->fullName,
+            'phone_number' => $request->phoneNumber,
+            'state' => $request->state,
+            'city' => $request->city,
+            'postal_code' => $request->postalCode,
+            'street_name' => $request->streetName,
+        ]);
+
+        return redirect()->back();
     }
 
     public function edit($id)
