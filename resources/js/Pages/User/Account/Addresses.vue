6c2c10e6e5df6cc769c19818d3264735a472f40f
Amrilazim3 <a.azim0711@gmail.com>
2022-06-09 17:48:38 +0800

    commit changes

--- resources/js/Pages/User/Account/Addresses.vue
+++ resources/js/Pages/User/Account/Addresses.vue
@@ -234,7 +234,7 @@
                                                 type="text"
                                                 autocomplete="phone-number"
                                                 required
-                                                placeholder="example: 60119872345 / 6011-987-2345"
+                                                placeholder="Example : 60119872345"
                                                 class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                                 v-model="
                                                     addressForm.phoneNumber
