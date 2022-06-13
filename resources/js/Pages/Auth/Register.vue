ebe45bb554d67606fdc529ee56b862992dec0c87
Amrilazim3 <a.azim0711@gmail.com>
2022-06-13 14:49:05 +0800

    disable button when user submit the form

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -138,12 +138,12 @@
                     <div>
                         <button
                             type="submit"
-                            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white"
                             :disabled="form.processing"
                             :class="
                                 form.processing
-                                    ? 'cursor-not-allowed bg-indigo-700'
-                                    : ''
+                                    ? 'bg-indigo-300 cursor-not-allowed'
+                                    : 'bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                             "
                         >
                             create an account
