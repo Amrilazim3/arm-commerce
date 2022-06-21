51425b1decf809ddebdce6b08c4a8d1015402a13
Amrilazim3 <a.azim0711@gmail.com>
2022-06-21 13:09:36 +0800

    change naming false

--- resources/js/Shared/UserNav.vue
+++ resources/js/Shared/UserNav.vue
@@ -42,15 +42,15 @@
 
                     <MenuItem>
                         <Link
-                            href="/user/account/payment"
+                            href="/user/account/payments"
                             :class="[
                                 'group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-violet-500 hover:text-gray-100 text-gray-900',
-                                $page.component == 'User/Account/Payment'
+                                $page.component == 'User/Account/Payments'
                                     ? 'bg-violet-500 text-gray-100'
                                     : '',
                             ]"
                         >
-                            Bank & Cards
+                            Banks and Cards
                         </Link>
                     </MenuItem>
 
@@ -240,14 +240,14 @@
                         </li>
                         <li>
                             <Link
-                                href="/user/account/payment"
+                                href="/user/account/payments"
                                 :class="
-                                    $page.component == 'User/Account/Payment'
+                                    $page.component == 'User/Account/Payments'
                                         ? 'font-bold'
                                         : ''
                                 "
                             >
-                                Bank & Cards
+                                Banks and Cards
                             </Link>
                         </li>
                         <li>
