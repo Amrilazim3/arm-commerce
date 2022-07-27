847e43f80d5c608ff7132e9bc09aab7df0c5ee59
Amrilazim3 <a.azim0711@gmail.com>
2022-07-28 07:48:50 +0800

    make the rest of user has role of user

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
@@ -14,7 +15,17 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'user']);
+        // Role::create(['name' => 'admin']);
+        // Role::create(['name' => 'user']);
+
+        // assign certain user to become admin
+        // $user = User::where('email', 'a.azim0711@gmail.com')->first();
+        // $user->assignRole('admin');
+
+        // assign certain user to become user
+        // $users = User::where('id', '!=', 11)->lazy();
+        // foreach ($users as $user) {
+        //     $user->assignRole('user');
+        // }
     }
 }
