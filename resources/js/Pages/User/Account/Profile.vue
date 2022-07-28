f4131fd8f0afd16c7910ef9f2efb2e37c8a0f4c5
Amrilazim3 <a.azim0711@gmail.com>
2022-07-29 00:22:47 +0800

    calling SideNav component

--- resources/js/Pages/User/Account/Profile.vue
+++ resources/js/Pages/User/Account/Profile.vue
@@ -1,7 +1,7 @@
 <template>
     <Head title="Profile" />
     <div class="lg:flex">
-        <UserNav />
+        <SideNav />
         <form
             class="px-10 lg:pl-10 lg:pr-28 py-6 space-y-8 divide-y divide-gray-200 lg:flex-1"
             @submit.prevent="updateProfile"
@@ -315,11 +315,11 @@
 </template>
 
 <script>
-import UserNav from "../../../Shared/UserNav.vue";
+import SideNav from "../../../Shared/SideNav.vue";
 
 export default {
     components: {
-        UserNav,
+        SideNav,
     },
 
     data() {
