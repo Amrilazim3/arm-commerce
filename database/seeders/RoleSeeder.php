40b6eef9e29409adc9cd2f53cce3a41ad52a07dc
Amrilazim3 <a.azim0711@gmail.com>
2022-07-28 09:55:47 +0800

    commit changes

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,17 +15,17 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        // Role::create(['name' => 'admin']);
-        // Role::create(['name' => 'user']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
 
         // assign certain user to become admin
-        // $user = User::where('email', 'a.azim0711@gmail.com')->first();
-        // $user->assignRole('admin');
+        $user = User::where('email', 'a.azim0711@gmail.com')->first();
+        $user->assignRole('admin');
 
         // assign certain user to become user
-        // $users = User::where('id', '!=', 11)->lazy();
-        // foreach ($users as $user) {
-        //     $user->assignRole('user');
-        // }
+        $users = User::where('id', '!=', 11)->lazy();
+        foreach ($users as $user) {
+            $user->assignRole('user');
+        }
     }
 }
