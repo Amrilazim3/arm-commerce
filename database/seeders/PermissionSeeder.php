b8fc184d475cf8505bad95ea0ddcde098bc17aaf
Amrilazim3 <a.azim0711@gmail.com>
2022-07-28 09:56:30 +0800

    create permissions for admin and assign to admin role

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class PermissionSeeder extends Seeder
 {
@@ -13,6 +15,21 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $adminPermissions = [
+            'create product',
+            'edit product',
+            'delete product'
+        ];
+
+        foreach ($adminPermissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $role = Role::where('name', 'admin')->first();
+
+        // assign adminPermissions to role admin.
+        foreach ($adminPermissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
     }
 }
