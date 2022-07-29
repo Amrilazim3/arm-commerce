b73a3943b5815aec4d2c07d4c1e96908bedb7ffc
Amrilazim3 <a.azim0711@gmail.com>
2022-07-29 10:38:03 +0800

    create ChangePasswordController for admin

---
+++ app/Http/Controllers/Admin/Account/ChangePasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Account;
+
+use App\Http\Controllers\Controller;
+use App\Rules\VerifyNewPassword;
+use App\Rules\VerifyPassword;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class ChangePasswordController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Admin/Account/ChangePassword');
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $request->validate([
+            'oldPassword' => ['required', new VerifyPassword($user->email)],
+            'newPassword' => ['required', 'min:7', new VerifyNewPassword($user->email)],
+            'newPasswordConfirmation' => ['required', 'same:newPassword']
+        ]);
+
+        $user->update([
+            'password' => $request->newPassword
+        ]);
+
+        return redirect()->route('products.index');
+    }
+}
