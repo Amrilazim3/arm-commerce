d2a28034a8146fefef2cd9586e41c578e264e2ad
Amrilazim3 <a.azim0711@gmail.com>
2022-07-29 10:37:38 +0800

    create ChangeEmailController for admin

---
+++ app/Http/Controllers/Admin/Account/ChangeEmailController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Account;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\SendEmailVerificationNotificationJob;
+use App\Rules\VerifyPassword;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class ChangeEmailController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Admin/Account/ChangeEmail');
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $request->validate([
+            'currentEmail' => ['required', 'email', 'exists:users,email'],
+            'newEmail' => ['required', 'email', 'unique:users,email,' . $user->id],
+            'password' => ['required', new VerifyPassword($user->email)],
+        ]);
+
+        $user->update([
+            'email' => $request->newEmail,
+            'email_verified_at' => null
+        ]);
+
+        SendEmailVerificationNotificationJob::dispatch($user);
+
+        return redirect()->route('products.index')->with('success', 'email verification link has been sent');
+    }
+}
