105bc9e8b9e0d75dec413711919b6b0d4d0dec35
Amrilazim3 <a.azim0711@gmail.com>
2022-08-22 10:13:27 +0800

    adding style to editor class attribute

--- resources/js/Shared/Tiptap.vue
+++ resources/js/Shared/Tiptap.vue
@@ -6,6 +6,7 @@
             v-if="editor"
             class="border rounded-md px-2 py-0.5 border-gray-800 bg-white space-x-2"
         >
+            <!-- Bullet List -->
             <button
                 @click="editor.chain().focus().toggleBulletList().run()"
                 class="px-0.5 border rounded-md hover:border-gray-600"
@@ -13,6 +14,7 @@
             >
                 Bullet List
             </button>
+            <!-- Ordered List -->
             <button
                 @click="editor.chain().focus().toggleOrderedList().run()"
                 class="px-0.5 border rounded-md hover:border-gray-600"
@@ -20,6 +22,7 @@
             >
                 Ordered List
             </button>
+            <!-- Bold -->
             <button
                 @click="editor.chain().focus().toggleBold().run()"
                 class="px-0.5 border rounded-md hover:border-gray-600"
@@ -85,7 +88,7 @@ export default {
             ],
             editorProps: {
                 attributes: {
-                    class: "form-control prose prose-sm block w-full lg:w-3/4 px-3 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-md transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-indigo-600 focus:outline-none",
+                    class: "form-control prose prose-sm block w-full h-52 overflow-y-scroll px-3 py-2 text-base font-normal text-gray-700 bg-white bg-clip-padding border-2 border-solid border-gray-300 rounded-md transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-indigo-600 focus:outline-none",
                 },
             },
             content: this.modelValue,
