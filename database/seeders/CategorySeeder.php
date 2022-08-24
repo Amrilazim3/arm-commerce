a440e324d9c78d2c6a98777cf8f3d6929f86c83a
Amrilazim3 <a.azim0711@gmail.com>
2022-08-25 07:04:50 +0800

    Create seeder for Category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'Fitness',
+            'Gym',
+            'Supplement',
+            'Lifestyle',
+            'Fit'
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category)
+            ]);
+        }
+    }
+}
