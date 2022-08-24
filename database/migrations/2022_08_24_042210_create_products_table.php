a8f6a0b684fcaba3e111ae8f4090775cf2bd3163
Amrilazim3 <a.azim0711@gmail.com>
2022-08-24 15:25:09 +0800

    create product table

---
+++ database/migrations/2022_08_24_042210_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); // who creating the product
+            $table->foreignId('category_id')->constrained();
+            $table->string('name'); // name of product
+            $table->string('slug')->unique(); // for generating unique url 
+            $table->longText('description'); // use rich text editor 
+            $table->unsignedBigInteger('stock'); // overall stock (also count the variation stock)
+            $table->unsignedDouble('price'); // if product doesn't have any variants
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
