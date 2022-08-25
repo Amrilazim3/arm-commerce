a9df48cb3cef1c1332357d62f87c8225aeb7b639
Amrilazim3 <a.azim0711@gmail.com>
2022-08-25 09:17:59 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class
+        ]); // can call multiple seeder in one call.
     }
 }
