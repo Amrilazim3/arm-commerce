2dae4863ad85f5870e71bbf4d03cb783106ffdb3
Amrilazim3 <a.azim0711@gmail.com>
2022-08-27 15:55:53 +0800

    creating local scope for sorting products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,6 +11,17 @@ class Product extends Model
 
     protected $guarded = [];
 
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['created_at'] ?? false, function ($query, $created_at) {
+            $query->orderBy('created_at', $created_at);
+        });
+
+        $query->when($filters['stock'] ?? false, function ($query, $stock) {
+            $query->orderBy('stock', $stock);
+        });
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id', 'id');
