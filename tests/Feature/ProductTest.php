11e1ff190b51602f153a9aab3565eafd656e14f9
Amrilazim3 <a.azim0711@gmail.com>
2022-08-27 21:46:28 +0800

    creating Product test file

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_post_request()
+    {
+        $user = User::where('email', 'a.azim0711@gmail.com')->first();
+
+        $response = $this->actingAs($user)->post("admin/products", [
+            "name" => "jog shoes",
+            "description" => "page editors It is a long established fact that a reader will be distracted by the readable content of a page It is a long established fact that a reader will be distracted by the readable content of a page It is a long established fact that a reader will be distracted by the readable content of a page It is a long established fact that a reader will be distracted by the readable content of a page",
+            "category" => "Shoes",
+            "stock" => 20,
+            "price" => 120,
+            "media" => []
+        ]);
+
+        $response->assertSessionHasNoErrors();
+        // $response->assertStatus(301);
+    }
+}
