b28e85ae639a4a8aa2669b5d6e170f6f2d4dc372
Amrilazim3 <a.azim0711@gmail.com>
2022-08-30 17:14:35 +0800

    comment options part

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,8 @@
 <template>
     <Head title="Login" />
-    <div class="bg-gray-100 min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div
+        class="bg-gray-100 min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8"
+    >
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <img
                 class="mx-auto h-12 w-auto"
@@ -14,100 +16,64 @@
 
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form class="space-y-6" @submit.prevent="login">
-                    <div>
-                        <label
-                            for="email"
-                            class="block text-sm font-medium text-gray-700"
-                        >
-                            Email address
-                        </label>
-                        <div class="mt-1">
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                autocomplete="email"
-                                required=""
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                v-model="form.email"
-                            />
-                        </div>
-                        <div
-                            v-if="form.errors.email"
-                            class="text-red-500 text-sm"
-                        >
-                            {{ form.errors.email }}
-                        </div>
-                    </div>
+                <FormKit
+                    type="form"
+                    form-label="Log in"
+                    form-class="space-y-6"
+                    :submit-attrs="{
+                        inputClass: 'w-full'
+                    }"
+                    :disabled="form.processing"
+                    @submit="login"
+                    :input-errors="{
+                        email: form.errors.email ? form.errors.email : '',
+                        password: form.errors.password ? form.errors.password : ''
+                    }"
+                >
+                    <FormKit
+                        name="email"
+                        label="Email address"
+                        type="text"
+                        validation="required|email"
+                        placeholder="Enter your email address"
+                        v-model="form.email"
+                    />
 
-                    <div>
-                        <label
-                            for="password"
-                            class="block text-sm font-medium text-gray-700"
-                        >
-                            Password
-                        </label>
-                        <div class="mt-1">
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autocomplete="current-password"
-                                required=""
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                v-model="form.password"
-                            />
-                        </div>
-                        <div
-                            v-if="form.errors.password"
-                            class="text-red-500 text-sm"
-                        >
-                            {{ form.errors.password }}
-                        </div>
-                    </div>
+                    <FormKit
+                        name="password"
+                        label="Password"
+                        type="password"
+                        validation="required"
+                        placeholder="Enter your password"
+                        v-model="form.password"
+                    />
 
                     <div class="flex items-center justify-between">
-                        <div class="flex items-center">
-                            <input
-                                id="remember-me"
-                                name="remember-me"
-                                type="checkbox"
-                                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                v-model="form.isRememberMe"
-                            />
-                            <label
-                                for="remember-me"
-                                class="ml-2 block text-sm text-gray-900"
-                            >
-                                Remember me
-                            </label>
-                        </div>
+                        <FormKit
+                            outer-class="$reset"
+                            label="Remember me"
+                            type="checkbox"
+                            v-model="form.isRememberMe"
+                        />
 
-                        <div class="text-sm">
-                            <Link
-                                href="/password/forgot"
-                                class="font-medium text-indigo-600 hover:text-indigo-500"
-                            >
-                                Forgot your password?
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div>
-                        <button
-                            type="submit"
-                            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white"
-                            :disabled="form.processing"
-                            :class="form.processing ? 'bg-indigo-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'"
+                        <Link
+                            href="/password/forgot"
+                            class="font-medium text-sm text-indigo-600 hover:text-indigo-500"
                         >
-                            Log in
-                        </button>
+                            Forgot your password?
+                        </Link>
                     </div>
 
-                    <p class="text-sm font-medium text-green-500" v-if="$page.props.flash.success == 'Your password has been reset!'">{{ $page.props.flash.success }}</p>
-                </form>
-
+                    <p
+                        class="text-sm font-medium text-green-500"
+                        v-if="
+                            $page.props.flash.success ==
+                            'Your password has been reset!'
+                        "
+                    >
+                        {{ $page.props.flash.success }}
+                    </p>
+                </FormKit>
                 <ThirdPartyServices />
             </div>
         </div>
@@ -121,7 +87,7 @@ export default {
     layout: null,
 
     components: {
-        ThirdPartyServices
+        ThirdPartyServices,
     },
 
     data() {
