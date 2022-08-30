3779180b4ab2a0ddfd148489291666d9f1ac1b87
Amrilazim3 <a.azim0711@gmail.com>
2022-08-30 17:11:08 +0800

    import into plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const formKitTailwind = require('@formkit/themes/tailwindcss');
+
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
@@ -12,5 +14,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('autoprefixer'),
         require('@tailwindcss/typography'),
+        formKitTailwind
     ],
 }
