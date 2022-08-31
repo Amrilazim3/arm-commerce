0598d684d4eac1b8becefa7bd83e73249973e5b3
Amrilazim3 <a.azim0711@gmail.com>
2022-08-31 10:56:08 +0800

    remove tailwind cdn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8" />
     <link rel="icon" href="https://arm-commerce.com/images/dumbbell-solid.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <script src="{{ mix('/js/manifest.js') }}" defer></script>
