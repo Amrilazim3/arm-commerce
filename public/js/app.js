bb8e02456ee2962fb2b9ef08fdd42fc150a87902
Amrilazim3 <a.azim0711@gmail.com>
2022-08-31 11:03:39 +0800

    commit changes

--- public/js/app.js
+++ public/js/app.js
@@ -27,7 +27,7 @@ var boxClassification = {
   legend: 'font-bold text-sm',
   wrapper: 'flex items-center cursor-pointer',
   help: 'mb-2',
-  input: 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded',
+  input: 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded-full',
   label: 'ml-2 block text-sm text-semibold text-gray-900'
 };
 var buttonClassification = {
@@ -39,14 +39,14 @@ var buttonClassification = {
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   // the global key will apply to all inputs
   global: {
-    outer: 'mb-5 formkit-disabled:opacity-50',
+    outer: 'formkit-disabled:opacity-50',
     help: 'text-xs text-gray-500',
     messages: 'list-none p-0 mt-1 mb-0',
     message: 'text-red-500 mb-1 text-sm'
   },
   button: buttonClassification,
   color: {
-    label: 'block mb-1 font-bold text-sm',
+    label: 'block mb-1 font-semibold text-sm',
     input: 'w-16 h-8 appearance-none cursor-pointer border border-indigo-300 rounded-md mb-2 p-1'
   },
   date: textClassification,
@@ -59,7 +59,7 @@ var buttonClassification = {
     input: 'text-gray-600 text-sm mb-1 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:bg-indigo-500 file:text-white hover:file:bg-indigo-600',
     noFiles: 'block text-gray-800 text-sm mb-1',
     fileItem: 'block flex text-gray-800 text-sm mb-1',
-    fileRemove: 'ml-auto text-indigo-500 text-sm'
+    fileRemove: 'ml-auto text-blue-500 text-sm'
   },
   month: textClassification,
   number: textClassification,
