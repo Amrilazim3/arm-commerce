e777c08ef56b65d54eb5c8356ffa18bf8c254267
Amrilazim3 <a.azim0711@gmail.com>
2022-09-03 22:13:08 +0800

    create job for clearing temp image

---
+++ app/Jobs/ClearTempStorage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class ClearTempStorage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle()
+    {
+        File::cleanDirectory(Storage::disk('public')->path('temp'));
+    }
+}
