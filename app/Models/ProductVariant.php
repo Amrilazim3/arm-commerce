122e7b77726f42651205e14b7402e4c13b19ff0b
Amrilazim3 <a.azim0711@gmail.com>
2022-09-19 10:24:56 +0800

    adding relationship method

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -10,4 +10,14 @@ class ProductVariant extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function productDetails()
+    {
+        return $this->hasMany(ProductDetail::class);
+    }
 }
