7aabe5196769cc4244212483ee8ec408b561e7c6
Amrilazim3 <a.azim0711@gmail.com>
2022-10-07 09:12:32 +0800

    adding method show for showing single post

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,4 +29,87 @@ class ProductController extends Controller
             'productsData' => $productsData
         ]);
     }
+
+    public function show(Product $product)
+    {
+        $imagesObj = [];
+        $i = 1;
+        foreach ($product->images as $key => &$image) {
+            $imagesObj[$key]['id'] =  $i;
+            $imagesObj[$key]['url'] = $image->url;
+            $i++;
+        }
+
+        $optionsValues = [];
+        $variants = [];
+        $product->productVariants()->each(
+            function ($productVariantItem) use (&$variants, &$optionsValues) {
+                $variants[] = [
+                    'name' => $productVariantItem->name,
+                    'image_url' => $productVariantItem->image_url,
+                    'stock' => $productVariantItem->stock,
+                    'price' => $productVariantItem->price,
+                ];
+
+                $optionsLimit = count($productVariantItem->productDetails);
+
+                $productVariantItem->productDetails()->each(
+                    function ($productDetailItem) use (&$optionsValues, $optionsLimit) {
+                        $productOptionValue = $productDetailItem->variantValue->value;
+                        $productOptionName = $productDetailItem->variantValue->variant->name;
+
+                        if (empty($optionsValues)) {
+                            $optionsValues[] = [
+                                'name' => $productOptionName,
+                                'values' => [$productOptionValue],
+                            ];
+                            return;
+                        }
+
+                        for ($i = 0; $i < $optionsLimit; $i++) {
+                            if ($optionsValues[$i]['name'] === $productOptionName) {
+                                if (!in_array($productOptionValue, $optionsValues[$i]['values'])) {
+                                    array_push($optionsValues[$i]['values'], $productOptionValue);
+                                    return;
+                                }
+                                return;
+                            }
+
+                            if ($optionsValues[$i]['name'] !== $productOptionName) {
+                                if (!array_key_exists($i + 1, $optionsValues) && $i + 1 < $optionsLimit) {
+                                    $optionsValues[] = [
+                                        'name' => $productOptionName,
+                                        'values' => [$productOptionValue],
+                                    ];
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                );
+            }
+        );
+
+        for ($i = 0; $i < count($optionsValues); $i++) {
+            array_push($optionsValues[$i]["values"], "");
+        }
+
+        return Inertia::render('Products/Show', [
+            'productData' => [
+                'name' => $product->name,
+                'slug' => $product->slug,
+                'description' => $product->description,
+                'stock' => $product->stock,
+                'price' => $product->price,
+                'category' => [
+                    'id' => $product->category->id,
+                    'name' => $product->category->name,
+                    'slug' => $product->category->slug
+                ],
+                'images' => $imagesObj,
+                'options' => $optionsValues,
+                'variants' => $variants
+            ]
+        ]);
+    }
 }
