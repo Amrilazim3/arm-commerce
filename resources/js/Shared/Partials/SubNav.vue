c0776b49623abd0902692386723abcdea7397278
Amrilazim3 <a.azim0711@gmail.com>
2022-10-15 18:52:56 +0800

    add cart slide over in user role

--- resources/js/Shared/Partials/SubNav.vue
+++ resources/js/Shared/Partials/SubNav.vue
@@ -1,10 +1,4 @@
 <template>
-    <!-- 
-        apply for "desktop" layout only 
-        if user browsing in mobile size, 
-        this content will be apply in MainNav component
-    -->
-
     <nav class="hidden px-6 py-4 text-white bg-gray-700 md:block">
         <div class="flex justify-between">
             <svg
@@ -41,20 +35,11 @@
                         </Link>
                     </template>
                     <template v-else>
-                        <!-- public/cart-shopping-solid.svg -->
-                        <!-- the reason we use the svg tag because we can directly change the icon color -->
-                        <Link href="/user/account/profile">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 576 512"
-                                class="h-5 text-white fill-current"
-                            >
-                                <path
-                                    d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"
-                                />
-                            </svg>
-                        </Link>
-                        <span>0</span>
+                        <ShoppingCartIcon
+                            @click="isOpenCartSlideOver = true"
+                            class="h-6 w-6 self-center cursor-pointer"
+                        />
+                        <span class="font-semibold">0</span>
                     </template>
                 </template>
                 <template v-else>
@@ -74,8 +59,260 @@
             </div>
         </div>
     </nav>
+
+    <TransitionRoot as="template" :show="isOpenCartSlideOver">
+        <Dialog as="div" class="relative z-10" @close="isOpenCartSlideOver = false">
+            <TransitionChild
+                as="template"
+                enter="ease-in-out duration-500"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="ease-in-out duration-500"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+            >
+                <div
+                    class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                />
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-hidden">
+                <div class="absolute inset-0 overflow-hidden">
+                    <div
+                        class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="transform transition ease-in-out duration-500 sm:duration-700"
+                            enter-from="translate-x-full"
+                            enter-to="translate-x-0"
+                            leave="transform transition ease-in-out duration-500 sm:duration-700"
+                            leave-from="translate-x-0"
+                            leave-to="translate-x-full"
+                        >
+                            <DialogPanel
+                                class="pointer-events-auto w-screen max-w-md"
+                            >
+                                <div
+                                    class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl"
+                                >
+                                    <div
+                                        class="flex-1 overflow-y-auto py-6 px-4 sm:px-6"
+                                    >
+                                        <div
+                                            class="flex items-start justify-between"
+                                        >
+                                            <DialogTitle
+                                                class="text-lg font-medium text-gray-900"
+                                                >Shopping cart</DialogTitle
+                                            >
+                                            <div
+                                                class="ml-3 flex h-7 items-center"
+                                            >
+                                                <button
+                                                    type="button"
+                                                    class="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                                                    @click="isOpenCartSlideOver = false"
+                                                >
+                                                    <span class="sr-only"
+                                                        >Close panel</span
+                                                    >
+                                                    <XIcon
+                                                        class="h-6 w-6"
+                                                        aria-hidden="true"
+                                                    />
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                        <div class="mt-8">
+                                            <div class="flow-root">
+                                                <ul
+                                                    role="list"
+                                                    class="-my-6 divide-y divide-gray-200"
+                                                >
+                                                    <li
+                                                        v-for="product in products"
+                                                        :key="product.id"
+                                                        class="flex py-6"
+                                                    >
+                                                        <div
+                                                            class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200"
+                                                        >
+                                                            <img
+                                                                :src="
+                                                                    product.imageSrc
+                                                                "
+                                                                :alt="
+                                                                    product.imageAlt
+                                                                "
+                                                                class="h-full w-full object-cover object-center"
+                                                            />
+                                                        </div>
+
+                                                        <div
+                                                            class="ml-4 flex flex-1 flex-col"
+                                                        >
+                                                            <div>
+                                                                <div
+                                                                    class="flex justify-between text-base font-medium text-gray-900"
+                                                                >
+                                                                    <h3>
+                                                                        <a
+                                                                            :href="
+                                                                                product.href
+                                                                            "
+                                                                            >{{
+                                                                                product.name
+                                                                            }}</a
+                                                                        >
+                                                                    </h3>
+                                                                    <p
+                                                                        class="ml-4"
+                                                                    >
+                                                                        {{
+                                                                            product.price
+                                                                        }}
+                                                                    </p>
+                                                                </div>
+                                                                <p
+                                                                    class="mt-1 text-sm text-gray-500"
+                                                                >
+                                                                    {{
+                                                                        product.color
+                                                                    }}
+                                                                </p>
+                                                            </div>
+                                                            <div
+                                                                class="flex flex-1 items-end justify-between text-sm"
+                                                            >
+                                                                <p
+                                                                    class="text-gray-500"
+                                                                >
+                                                                    Qty
+                                                                    {{
+                                                                        product.quantity
+                                                                    }}
+                                                                </p>
+
+                                                                <div
+                                                                    class="flex"
+                                                                >
+                                                                    <button
+                                                                        type="button"
+                                                                        class="font-medium text-indigo-600 hover:text-indigo-500"
+                                                                    >
+                                                                        Remove
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div
+                                        class="border-t border-gray-200 py-6 px-4 sm:px-6"
+                                    >
+                                        <div
+                                            class="flex justify-between text-base font-medium text-gray-900"
+                                        >
+                                            <p>Subtotal</p>
+                                            <p>$262.00</p>
+                                        </div>
+                                        <p class="mt-0.5 text-sm text-gray-500">
+                                            Shipping and taxes calculated at
+                                            checkout.
+                                        </p>
+                                        <div class="mt-6">
+                                            <a
+                                                href="#"
+                                                class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                                                >Checkout</a
+                                            >
+                                        </div>
+                                        <div
+                                            class="mt-6 flex justify-center text-center text-sm text-gray-500"
+                                        >
+                                            <p>
+                                                or
+                                                <button
+                                                    type="button"
+                                                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                                                    @click="isOpenCartSlideOver = false"
+                                                >
+                                                    Continue Shopping
+                                                    <span aria-hidden="true">
+                                                        &rarr;</span
+                                                    >
+                                                </button>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
 </template>
 
 <script>
-export default {};
+import {
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    TransitionChild,
+    TransitionRoot,
+} from "@headlessui/vue";
+import { XIcon } from "@heroicons/vue/outline";
+import { ShoppingCartIcon } from "@heroicons/vue/solid";
+
+export default {
+    components: {
+        Dialog,
+        DialogPanel,
+        DialogTitle,
+        TransitionChild,
+        TransitionRoot,
+        XIcon,
+        ShoppingCartIcon,
+    },
+
+    data() {
+        return {
+            products: [
+                {
+                    id: 1,
+                    name: "Throwback Hip Bag",
+                    href: "#",
+                    color: "Salmon",
+                    price: "$90.00",
+                    quantity: 1,
+                    imageSrc:
+                        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
+                    imageAlt:
+                        "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
+                },
+                {
+                    id: 2,
+                    name: "Medium Stuff Satchel",
+                    href: "#",
+                    color: "Blue",
+                    price: "$32.00",
+                    quantity: 1,
+                    imageSrc:
+                        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+                    imageAlt:
+                        "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
+                },
+            ],
+            isOpenCartSlideOver: false,
+        };
+    },
+};
 </script>
