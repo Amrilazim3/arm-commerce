3a11e22572b75b73efd97e32b2770d1ce18e85a1
Amrilazim3 <a.azim0711@gmail.com>
2022-10-16 15:21:43 +0800

    change cart icon

--- resources/js/Shared/Partials/MainNav.vue
+++ resources/js/Shared/Partials/MainNav.vue
@@ -329,9 +329,21 @@
                                             <div
                                                 class="flex space-x-2 self-center"
                                             >
-                                                <template v-if="$page.props.auth.isLoggedIn">
-                                                    <template v-if="$page.props.auth.user.isAdmin">
-                                                        <Link href="/admin/products">
+                                                <template
+                                                    v-if="
+                                                        $page.props.auth
+                                                            .isLoggedIn
+                                                    "
+                                                >
+                                                    <template
+                                                        v-if="
+                                                            $page.props.auth
+                                                                .user.isAdmin
+                                                        "
+                                                    >
+                                                        <Link
+                                                            href="/admin/products"
+                                                        >
                                                             <svg
                                                                 xmlns="http://www.w3.org/2000/svg"
                                                                 viewBox="0 0 640 512"
@@ -344,33 +356,25 @@
                                                         </Link>
                                                     </template>
                                                     <template v-else>
-                                                        <Link href="/user/account/profile">
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                viewBox="0 0 576 512"
-                                                                class="h-5 text-white fill-current"
-                                                            >
-                                                                <path
-                                                                    d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"
-                                                                />
-                                                            </svg>
-                                                        </Link>
-                                                        <span>0</span>
+                                                        <ShoppingCartIcon
+                                                            class="h-6 w-6 self-center cursor-pointer"
+                                                        />
+                                                        <span
+                                                            class="font-semibold"
+                                                            >0</span
+                                                        >
                                                     </template>
                                                 </template>
                                                 <template v-else>
                                                     <Link href="/login">
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            viewBox="0 0 576 512"
-                                                            class="h-5 text-white fill-current"
-                                                        >
-                                                            <path
-                                                                d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"
-                                                            />
-                                                        </svg>
+                                                        <ShoppingCartIcon
+                                                            class="h-6 w-6 self-center cursor-pointer"
+                                                        />
                                                     </Link>
-                                                    <span>0</span>
+                                                    <span
+                                                        class="font-semibold"
+                                                        >0</span
+                                                    >
                                                 </template>
                                             </div>
                                         </nav>
@@ -382,7 +386,12 @@
                                         v-if="$page.props.auth.isLoggedIn"
                                     >
                                         <div class="grid grid-cols-1 gap-7">
-                                            <template v-if="$page.props.auth.user.isAdmin">
+                                            <template
+                                                v-if="
+                                                    $page.props.auth.user
+                                                        .isAdmin
+                                                "
+                                            >
                                                 <Link
                                                     href="/admin/account/profile"
                                                     class="flex items-center p-3 -m-3 rounded-lg hover:bg-gray-400"
@@ -504,6 +513,7 @@ import {
     MenuItems,
     MenuItem,
 } from "@headlessui/vue";
+import { ShoppingCartIcon } from "@heroicons/vue/solid";
 
 export default {
     components: {
@@ -517,6 +527,8 @@ export default {
         MenuButton,
         MenuItems,
         MenuItem,
+
+        ShoppingCartIcon,
     },
 };
 </script>
