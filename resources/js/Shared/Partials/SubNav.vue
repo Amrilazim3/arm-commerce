959b9263c0513f734c0236550a82036f3019d7fa
Amrilazim3 <a.azim0711@gmail.com>
2022-10-16 10:28:38 +0800

    showing dynamic data in cart slide over

--- resources/js/Shared/Partials/SubNav.vue
+++ resources/js/Shared/Partials/SubNav.vue
@@ -132,7 +132,9 @@
                                                     class="-my-6 divide-y divide-gray-200"
                                                 >
                                                     <li
-                                                        v-for="product in products"
+                                                        v-for="product in $page
+                                                            .props.auth.user
+                                                            .carts"
                                                         :key="product.id"
                                                         class="flex py-6"
                                                     >
@@ -141,10 +143,13 @@
                                                         >
                                                             <img
                                                                 :src="
-                                                                    product.imageSrc
+                                                                    product.imageUrl
+                                                                        ? product.imageUrl
+                                                                        : 'https://picsum.photos/200/300?random=' +
+                                                                          product.id
                                                                 "
                                                                 :alt="
-                                                                    product.imageAlt
+                                                                    product.name
                                                                 "
                                                                 class="h-full w-full object-cover object-center"
                                                             />
@@ -158,28 +163,27 @@
                                                                     class="flex justify-between text-base font-medium text-gray-900"
                                                                 >
                                                                     <h3>
-                                                                        <a
-                                                                            :href="
-                                                                                product.href
-                                                                            "
+                                                                        <Link
+                                                                            :href="'https://arm-commerce.com/products/' + product.slug"
+                                                                            @click="isOpenCartSlideOver = false"
                                                                             >{{
                                                                                 product.name
-                                                                            }}</a
+                                                                            }}</Link
                                                                         >
                                                                     </h3>
                                                                     <p
-                                                                        class="ml-4"
+                                                                        class="ml-4 text-sm font-light"
                                                                     >
                                                                         {{
                                                                             product.price
-                                                                        }}
+                                                                        }} MYR
                                                                     </p>
                                                                 </div>
                                                                 <p
                                                                     class="mt-1 text-sm text-gray-500"
                                                                 >
                                                                     {{
-                                                                        product.color
+                                                                        product.variantName
                                                                     }}
                                                                 </p>
                                                             </div>
@@ -220,7 +224,7 @@
                                             class="flex justify-between text-base font-medium text-gray-900"
                                         >
                                             <p>Subtotal</p>
-                                            <p>$262.00</p>
+                                            <p>262.00 MYR</p>
                                         </div>
                                         <p class="mt-0.5 text-sm text-gray-500">
                                             Shipping and taxes calculated at
@@ -287,34 +291,17 @@ export default {
 
     data() {
         return {
-            products: [
-                {
-                    id: 1,
-                    name: "Throwback Hip Bag",
-                    href: "#",
-                    color: "Salmon",
-                    price: "$90.00",
-                    quantity: 1,
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-                    imageAlt:
-                        "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-                },
-                {
-                    id: 2,
-                    name: "Medium Stuff Satchel",
-                    href: "#",
-                    color: "Blue",
-                    price: "$32.00",
-                    quantity: 1,
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-                    imageAlt:
-                        "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-                },
-            ],
             isOpenCartSlideOver: false,
+            cartSubtotal: 0,
         };
     },
+
+    mounted() {
+        var carts = this.$page.props.auth.user.carts;
+        // make a calculation for subtotal.
+        carts.forEach((item) => {
+            console.log(item.name);
+        });
+    }
 };
 </script>
