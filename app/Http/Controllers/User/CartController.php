25fb0f0f7e8875b8e4aaec1547b231940c3e9641
Amrilazim3 <a.azim0711@gmail.com>
2022-10-16 10:05:28 +0800

    return necessary data that need to be return in index method

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,9 +13,27 @@ class CartController extends Controller
 {
     public function index()
     {
-        $cart = Cart::where('user_id', auth()->user()->id)->get();
+        $carts = Cart::where('user_id', auth()->user()->id)->get();
+        
+        $cartsData = [];
+        foreach ($carts as $key => &$cart) {
+            $cartsData[$key]['id'] = $cart->id;
+            $cartsData[$key]['userId'] = $cart->user_id;
+            $cartsData[$key]['productId'] = $cart->product_id;
 
-        return $cart;
+            $product = Product::where('id', $cart->product_id)->first();
+            $cartsData[$key]['name'] = $product->name;
+            $cartsData[$key]['slug'] = $product->slug;
+
+            $cartsData[$key]['variantName'] = $cart->variant_name;
+            $cartsData[$key]['quantity'] = $cart->quantity;
+            $cartsData[$key]['isCheckout'] = $cart->is_checkout ? true : false;
+
+            $image = ProductImage::where('product_id', $cart->product_id)->first();
+            $cartsData[$key]['imageUrl'] = $image ? $image->url : null;
+        }
+
+        return $cartsData;
     }
 
     public function destroy()
