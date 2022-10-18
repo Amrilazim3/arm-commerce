1bb36821061bdce581760a6e959b175da6d28df9
Amrilazim3 <a.azim0711@gmail.com>
2022-10-18 22:58:25 +0800

    using pinia to interact with cart click

--- resources/js/Shared/Partials/Header.vue
+++ resources/js/Shared/Partials/Header.vue
@@ -3,17 +3,23 @@
         <MainNav />
 
         <SubNav />
+
+        <template v-if="$page.props.auth.isLoggedIn && !$page.props.auth.user.isAdmin">
+            <CartSlideOver />
+        </template>
     </header>
 </template>
 
 <script>
 import MainNav from "./MainNav.vue";
 import SubNav from "./SubNav.vue";
+import CartSlideOver from "../CartSlideOver.vue";
 
 export default {
     components: {
         MainNav,
-        SubNav
+        SubNav,
+        CartSlideOver
     },
 };
 </script>
