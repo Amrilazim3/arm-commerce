94f47703df96f9e842d9d5be864b56dc054ceb57
Amrilazim3 <a.azim0711@gmail.com>
2022-10-19 15:49:24 +0800

    adding method for getting cart products and perform a calculation for cart subtotal

--- resources/js/Stores/CartSliderStore.js
+++ resources/js/Stores/CartSliderStore.js
@@ -1,15 +1,33 @@
 import { defineStore } from "pinia";
+import axios from "axios"
 
 export const useCartSliderStore = defineStore('cartSliderStore', {
     state() {
         return {
-            isClicked: false
+            isClicked: false,
+            cartProducts: [],
+            cartSubtotal: 0,
         }
     },
 
     actions: {
+        async getCartProducts() {
+            try {
+                const result = await axios.get("https://arm-commerce.com/user/carts");
+                
+                this.cartProducts = result.data;
+
+                this.cartSubtotal = 0;
+                this.cartProducts.forEach((item) => {
+                    this.cartSubtotal += item.price * item.quantity;
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
         changeValue() {
             this.isClicked = !this.isClicked;
-        }
+        },
     }
 })
