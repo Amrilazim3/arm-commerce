62affe256d577d369c5c3e4aca415e75a01ef2e2
Amrilazim3 <a.azim0711@gmail.com>
2022-10-24 18:35:25 +0800

    create bill and send user to billplz payment page

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Http\Traits\StatesCitiesTrait;
 use App\Models\User;
+use Billplz\Client;
+use Billplz\Laravel\Billplz;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class CheckoutController extends Controller
 {
@@ -15,6 +18,8 @@ class CheckoutController extends Controller
 
     public function index()
     {
+        // get the user's cart and check if has product with is_checkout == true.
+
         $statesCitiesRes = $this->getStatesCitiesData();
 
         return Inertia::render('User/Checkout', [
@@ -22,5 +27,25 @@ class CheckoutController extends Controller
             'states' => $statesCitiesRes['states'],
             'statesCities' => $statesCitiesRes['statesCities']
         ]);
+    }
+
+    public function confirmOrder(Request $request)
+    {
+        $billplzBill = Billplz::bill();
+
+        $response = $billplzBill->create(
+            'bgs0cicq',
+            'a.azim0711@gmail.com',
+            '60175221202',
+            'Encik akmal',
+            1000,
+            'https://arm-commerce.com/products',
+            'testing billplz api',
+            [
+                'redirect_url' => 'https://arm-commerce.com/products?billplz_payment_information=success'
+            ]
+        );
+
+        return $response->toArray()['url'];
     }
 }
