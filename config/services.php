cf029c5f1748a2ce161e5f88623629836f24c391
Amrilazim3 <a.azim0711@gmail.com>
2022-10-24 17:53:31 +0800

    binding billplz configuration

--- config/services.php
+++ config/services.php
@@ -51,4 +51,11 @@ return [
     'abstractapi' => [
         'api_key' => env('ABSTRACTAPI_API_KEY')
     ],
+
+    'billplz' => [
+        'key' => env('BILLPLZ_API_KEY'),
+        'version' => env('BILLPLZ_VERSION', 'v3'),
+        'x-signature' => env('BILLPLZ_X_SIGNATURE'),
+        'sandbox' => env('BILLPLZ_SANDBOX', false),
+    ]
 ];
