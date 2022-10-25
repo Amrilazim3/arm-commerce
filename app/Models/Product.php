63b14b802d23f1aa84d886670d0f30340886de0b
Amrilazim3 <a.azim0711@gmail.com>
2022-10-25 20:06:40 +0800

    adding relationship with Cart model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -48,4 +48,9 @@ class Product extends Model
     {
         return $this->hasMany(ProductVariant::class, 'product_id');
     }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
