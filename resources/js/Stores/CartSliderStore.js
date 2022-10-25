8731776cda380e58fe6d86c68ed6f1a64406178d
Amrilazim3 <a.azim0711@gmail.com>
2022-10-25 11:10:01 +0800

    adding action for removing product

--- resources/js/Stores/CartSliderStore.js
+++ resources/js/Stores/CartSliderStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+const Swal = require('sweetalert2');
 import axios from "axios"
 
 export const useCartSliderStore = defineStore('cartSliderStore', {
@@ -14,7 +15,7 @@ export const useCartSliderStore = defineStore('cartSliderStore', {
         async getCartProducts() {
             try {
                 const result = await axios.get("https://arm-commerce.com/user/carts");
-                
+
                 this.cartProducts = result.data;
 
                 this.cartSubtotal = 0;
@@ -24,6 +25,27 @@ export const useCartSliderStore = defineStore('cartSliderStore', {
             } catch (error) {
                 console.log(error);
             }
+        },
+
+        async removeProduct(productId) {
+            const result = await axios.delete(`/user/carts/${productId}`);
+
+            if (result.data == 1) {
+                Swal.fire(
+                    "Success",
+                    "Product has been removed from the cart",
+                    "success"
+                );
+
+                this.getCartProducts();
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Failed",
+                    text: "Something went wrong! Please try again",
+                });
+            }
+
         },
 
         changeValue() {
