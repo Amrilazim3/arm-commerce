42012e33d265390e26ad76e672aa22de02559deb
Amrilazim3 <a.azim0711@gmail.com>
2022-10-25 20:07:06 +0800

    adding relationship with User and Product model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cart extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
