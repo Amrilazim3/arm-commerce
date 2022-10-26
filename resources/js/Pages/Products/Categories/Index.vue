7294a5f493f0f686d68592c778ede8bda767af89
Amrilazim3 <a.azim0711@gmail.com>
2022-10-26 20:44:39 +0800

    view products by category

---
+++ resources/js/Pages/Products/Categories/Index.vue
@@ -0,0 +1,37 @@
+<template>
+    <Head>
+        <title>Products</title>
+        <!-- <meta name="keywords" content="arm-commerce products" />
+        <meta
+            name="description"
+            content="Gym, fitness equipment and supplement"
+        /> -->
+    </Head>
+
+    <div class="bg-white">
+        <div
+            class="max-w-2xl mx-auto py-14 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
+        >
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+                {{ category }} products
+            </h2>
+
+            <ProductsLayout :productsData="productsData" />
+        </div>
+    </div>
+</template>
+
+<script>
+import ProductsLayout from '../../../Shared/ProductsLayout.vue';
+
+export default {
+    components: {
+        ProductsLayout
+    },
+
+    props: {
+        productsData: Object,
+        category: String,
+    },
+};
+</script>
