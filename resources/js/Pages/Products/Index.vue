546fc67bf558811d5c612e92383a35f6cbaa32a9
Amrilazim3 <a.azim0711@gmail.com>
2022-10-26 17:38:56 +0800

    calling ProductsLayout to display all products data

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -16,80 +16,24 @@
                 New Arrivals
             </h2>
 
-            <div
-                class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
-            >
-                <template v-for="product in products" :key="product.id">
-                    <div class="group relative">
-                        <div
-                            class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none"
-                        >
-                            <template v-if="product.images.length == 0">
-                                <img
-                                    :src="
-                                        'https://picsum.photos/200/300?random=' +
-                                        product.id
-                                    "
-                                    alt=""
-                                    class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                                />
-                            </template>
-                            <template v-else>
-                                <img
-                                    :src="product.images[0].url"
-                                    alt=""
-                                    class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                                />
-                            </template>
-                        </div>
-                        <div class="mt-4 flex justify-between">
-                            <div>
-                                <h3 class="text-sm text-gray-700">
-                                    <Link :href="'products/' + product.slug">
-                                        <span
-                                            aria-hidden="true"
-                                            class="absolute inset-0"
-                                        ></span>
-                                        {{ product.name }}
-                                    </Link>
-                                </h3>
-                                <p class="mt-1 text-sm text-indigo-500">
-                                    {{ product.stock }} unit in stock
-                                </p>
-                            </div>
-                            <p class="text-sm font-medium text-gray-900">
-                                {{ product.price }} MYR
-                            </p>
-                        </div>
-                    </div>
-                </template>
-            </div>
-            <Pagination class="flex justify-end" :links="productsData.links" />
+            <ProductsLayout :productsData="productsData" />
         </div>
     </div>
 </template>
 
 <script>
-import Pagination from "../../Shared/Pagination.vue";
+import ProductsLayout from "../../Shared/ProductsLayout.vue";
 
 export default {
     components: {
-        Pagination,
+        ProductsLayout
     },
 
     props: {
         productsData: Object,
     },
 
-    data() {
-        return {
-            products: [],
-        };
-    },
-
     mounted() {
-        this.products = this.productsData.data;
-
         if (this.$page.props.flash.success == "payment success") {
             this.$swal
                 .fire({
