9a0257ea064de23cef44efa4c4025efda51fe7bc
Amrilazim3 <a.azim0711@gmail.com>
2022-11-03 14:38:12 +0800

    query products data by search query

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,18 +11,39 @@ class CategoryController extends Controller
 {
     public function index(Category $category)
     {
-        $productsData = $category->products()->select([
-            'id',
-            'name',
-            'slug',
-            'description',
-            'stock',
-            'price'
-        ])
-            ->latest()
-            ->with(['images' => function ($query) {
-                return $query->select(['id', 'product_id', 'url']);
-            }])->paginate(20);
+        if (request('search')) {
+            $productsData = Product::select([
+                'id',
+                'name',
+                'slug',
+                'description',
+                'stock',
+                'price'
+            ])
+                ->filter(
+                    request(['search'])
+                )
+                ->where('category_id', $category->id)
+                ->with(['images' => function ($query) {
+                    return $query->select(['id', 'product_id', 'url']);
+                }])
+                ->latest()
+                ->paginate(20);
+        } else {
+            $productsData = $category->products()->select([
+                'id',
+                'name',
+                'slug',
+                'description',
+                'stock',
+                'price'
+            ])
+                ->with(['images' => function ($query) {
+                    return $query->select(['id', 'product_id', 'url']);
+                }])
+                ->latest()
+                ->paginate(20);
+        }
 
         $categories = Category::all(['id', 'name', 'slug']);
 
