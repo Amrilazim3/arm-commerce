4009a47f8ab96308df529a4d144d10714c960e2a
Amrilazim3 <a.azim0711@gmail.com>
2022-11-05 18:47:12 +0800

    add purchase folder

---
+++ app/Http/Controllers/User/Purchase/PaymentController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\User\Purchase;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PaymentController extends Controller
+{
+    public function index()
+    {
+        $ordersData = Order::select([
+            'id',
+            'cart_id',
+            'bill_id',
+            'bill_url',
+            'status',
+            'created_at'
+        ])
+            ->where('user_id', auth()->user()->id)
+            ->where('status', 'due')
+            ->with(['cart' => function ($item) {
+                return $item->select([
+                    'id',
+                    'product_id',
+                    'variant_name',
+                    'quantity',
+                    'price'
+                ])
+                    ->with(['product' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'name',
+                            'slug'
+                        ])->get();
+                    }])
+                    ->withTrashed();
+            }])
+            ->get();
+
+        return Inertia::render('User/Purchase/ToPay', [
+            'ordersData' => $ordersData
+        ]);
+    }
+}
