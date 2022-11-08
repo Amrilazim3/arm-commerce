0052d646ccffdca587b757d6cadc408d494b1049
Amrilazim3 <a.azim0711@gmail.com>
2022-11-08 14:28:04 +0800

    display message if no bill to pay

--- resources/js/Pages/User/Purchase/ToPay.vue
+++ resources/js/Pages/User/Purchase/ToPay.vue
@@ -16,128 +16,152 @@
                     </p>
                 </div>
 
-                <template v-for="bill in bills" :key="bill.billId">
-                    <div class="p-4">
-                        <div
-                            class="mx-auto w-full rounded border border-gray-300 divide-y divide-gray-200"
-                        >
-                            <div class="sm:flex justify-between p-5 bg-gray-50">
-                                <div class="flex space-x-6">
-                                    <div>
-                                        <h2 class="text-sm font-semibold">
-                                            Bill number
-                                        </h2>
-                                        <p class="text-sm text-gray-500">
-                                            {{ bill.billId }}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <h2 class="text-sm font-semibold">
-                                            Date placed
-                                        </h2>
-                                        <p class="text-sm text-gray-500">
-                                            {{ bill.createdAt }}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <h2 class="text-sm font-semibold">
-                                            Total amount
-                                        </h2>
-                                        <p class="text-sm text-gray-500">
-                                            {{ bill.total }}MYR
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="mt-4 sm:-mt-0">
-                                    <button
-                                        @click="openBill(bill.billId)"
-                                        class="rounded-md border border-gray-300 p-2.5 text-sm"
-                                    >
-                                        Pay Bill
-                                    </button>
-                                </div>
-                            </div>
-
-                            <template
-                                v-for="order in ordersData"
-                                :key="order.id"
+                <template v-if="ordersData.length == 0">
+                    <div class="flex items-center justify-center h-64">
+                        <h1 class="text-xl text-blue-500 font-semibold">
+                            No bills yet..
+                        </h1>
+                    </div>
+                </template>
+                <template v-else>
+                    <template v-for="bill in bills" :key="bill.billId">
+                        <div class="p-4">
+                            <div
+                                class="mx-auto w-full rounded border border-gray-300 divide-y divide-gray-200"
                             >
-                                <template v-if="order.bill_id == bill.billId">
-                                    <div class="p-5">
-                                        <div class="flex space-x-4">
-                                            <div class="w-36 h-36">
-                                                <img
-                                                    :src="
-                                                        'https://picsum.photos/200/100?random=' +
-                                                        order.id
-                                                    "
-                                                    alt="dummy"
-                                                    class="w-full h-full bg-gray-50 object-contain border rounded"
-                                                />
-                                            </div>
-                                            <div class="w-full">
-                                                <div
-                                                    class="flex mb-2 justify-between"
-                                                >
-                                                    <h3
-                                                        class="text-black text-base fontsem"
-                                                    >
-                                                        {{
-                                                            order.cart.product
-                                                                .name
-                                                        }}
-                                                    </h3>
-                                                    <h3
-                                                        class="text-black text-base fontsem"
-                                                    >
-                                                        {{
-                                                            order.cart.price /
-                                                            order.cart.quantity
-                                                        }}MYR
-                                                    </h3>
+                                <div
+                                    class="sm:flex justify-between p-5 bg-gray-50"
+                                >
+                                    <div class="flex space-x-6">
+                                        <div>
+                                            <h2 class="text-sm font-semibold">
+                                                Bill number
+                                            </h2>
+                                            <p class="text-sm text-gray-500">
+                                                {{ bill.billId }}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <h2 class="text-sm font-semibold">
+                                                Date placed
+                                            </h2>
+                                            <p class="text-sm text-gray-500">
+                                                {{ bill.createdAt }}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <h2 class="text-sm font-semibold">
+                                                Total amount
+                                            </h2>
+                                            <p class="text-sm text-gray-500">
+                                                {{ bill.total }}MYR
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 sm:-mt-0">
+                                        <button
+                                            @click="openBill(bill.billId)"
+                                            class="rounded-md border border-gray-300 p-2.5 text-sm"
+                                        >
+                                            Pay Bill
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <template
+                                    v-for="order in ordersData"
+                                    :key="order.id"
+                                >
+                                    <template
+                                        v-if="order.bill_id == bill.billId"
+                                    >
+                                        <div class="p-5">
+                                            <div class="flex space-x-4">
+                                                <div class="w-36 h-36">
+                                                    <img
+                                                        :src="
+                                                            'https://picsum.photos/200/100?random=' +
+                                                            order.id
+                                                        "
+                                                        alt="dummy"
+                                                        class="w-full h-full bg-gray-50 object-contain border rounded"
+                                                    />
                                                 </div>
-                                                <template
-                                                    v-if="
-                                                        order.cart.variant_name
-                                                    "
-                                                >
+                                                <div class="w-full">
+                                                    <div
+                                                        class="flex mb-2 justify-between"
+                                                    >
+                                                        <h3
+                                                            class="text-black text-base fontsem"
+                                                        >
+                                                            {{
+                                                                order.cart
+                                                                    .product
+                                                                    .name
+                                                            }}
+                                                        </h3>
+                                                        <h3
+                                                            class="text-black text-base fontsem"
+                                                        >
+                                                            {{
+                                                                order.cart
+                                                                    .price /
+                                                                order.cart
+                                                                    .quantity
+                                                            }}MYR
+                                                        </h3>
+                                                    </div>
+                                                    <template
+                                                        v-if="
+                                                            order.cart
+                                                                .variant_name
+                                                        "
+                                                    >
+                                                        <p
+                                                            class="text-sm text-gray-500"
+                                                        >
+                                                            variant :
+                                                            {{
+                                                                order.cart
+                                                                    .variant_name
+                                                            }}
+                                                        </p>
+                                                    </template>
                                                     <p
                                                         class="text-sm text-gray-500"
                                                     >
-                                                        variant :
+                                                        quantity :
                                                         {{
-                                                            order.cart
-                                                                .variant_name
+                                                            order.cart.quantity
                                                         }}
                                                     </p>
-                                                </template>
-                                                <p
-                                                    class="text-sm text-gray-500"
+                                                    <p
+                                                        class="text-sm text-gray-500"
+                                                    >
+                                                        total :
+                                                        {{
+                                                            order.cart.price
+                                                        }}MYR
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div class="mt-3 flex justify-end">
+                                                <Link
+                                                    :href="
+                                                        '/products/' +
+                                                        order.cart.product.slug
+                                                    "
+                                                    class="font-semibold text-sm text-indigo-500"
                                                 >
-                                                    quantity :
-                                                    {{ order.cart.quantity }}
-                                                </p>
-                                                <p
-                                                    class="text-sm text-gray-500"
-                                                >
-                                                    total :
-                                                    {{ order.cart.price }}MYR
-                                                </p>
+                                                    View product
+                                                </Link>
                                             </div>
                                         </div>
-                                        <div class="mt-3 flex justify-end">
-                                            <Link
-                                                :href="'/products/' + order.cart.product.slug"
-                                                class="font-semibold text-sm text-indigo-500"
-                                            >
-                                                View product
-                                            </Link>
-                                        </div>
-                                    </div>
+                                    </template>
                                 </template>
-                            </template>
+                            </div>
                         </div>
-                    </div>
+                    </template>
                 </template>
             </div>
         </div>
@@ -199,8 +223,9 @@ export default {
 
     methods: {
         openBill(billId) {
-            window.location.href = 'https://www.billplz-sandbox.com/bills/' + billId;
-        }
-    }
+            window.location.href =
+                "https://www.billplz-sandbox.com/bills/" + billId;
+        },
+    },
 };
 </script>
