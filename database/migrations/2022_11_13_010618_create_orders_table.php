e24309b8d48bf8f4cae0e28ea371025f2f8a84da
Amrilazim3 <a.azim0711@gmail.com>
2022-11-13 09:08:58 +0800

    recreate new orders table

---
+++ database/migrations/2022_11_13_010618_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('cart_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('address_id')->constrained()->cascadeOnDelete();
+            $table->string('bill_id');
+            $table->string('bill_url');
+            $table->string('contact_email');
+            $table->string('status')->default('due')->comment('due / paid / cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
