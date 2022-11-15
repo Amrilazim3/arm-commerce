048de4f06e12bc75922d59ce8863c3cfd63165a4
Amrilazim3 <a.azim0711@gmail.com>
2022-11-15 19:38:03 +0800

    add method to display user product to be receive

--- app/Http/Controllers/User/Purchase/ShippingController.php
+++ app/Http/Controllers/User/Purchase/ShippingController.php
@@ -19,13 +19,25 @@ class ShippingController extends Controller
             'created_at'
         ])
             ->where('user_id', auth()->user()->id)
+            ->where('status', 'pending')
             ->with(['order' => function ($item) {
                 return $item->select([
                     'id',
-                    'user_id',
                     'cart_id',
-                    'bill_id'
+                    'address_id',
+                    'contact_email'
                 ])
+                    ->with(['address' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'full_name',
+                            'phone_number',
+                            'state',
+                            'city',
+                            'postal_code',
+                            'street_name'
+                        ]);
+                    }])
                     ->with(['cart' => function ($item) {
                         return $item->select([
                             'id',
@@ -49,5 +61,59 @@ class ShippingController extends Controller
         return Inertia::render('User/Purchase/ToShip', [
             'shippingsData' => $shippingsData
         ]);
+    }
+
+    public function receive()
+    {
+        $shippingsData = Shipping::select([
+            'id',
+            'user_id',
+            'order_id',
+            'status',
+            'created_at'
+        ])
+            ->where('user_id', auth()->user()->id)
+            ->where('status', 'shipped')
+            ->with(['order' => function ($item) {
+                return $item->select([
+                    'id',
+                    'cart_id',
+                    'address_id',
+                    'contact_email'
+                ])
+                    ->with(['address' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'full_name',
+                            'phone_number',
+                            'state',
+                            'city',
+                            'postal_code',
+                            'street_name'
+                        ]);
+                    }])
+                    ->with(['cart' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'product_id',
+                            'variant_name',
+                            'quantity',
+                            'price'
+                        ])
+                            ->with(['product' => function ($item) {
+                                return $item->select([
+                                    'id',
+                                    'name',
+                                    'slug'
+                                ])->get();
+                            }])
+                            ->withTrashed();
+                    }]);
+            }])
+            ->get();
+    
+            return Inertia::render('User/Purchase/ToReceive', [
+                'shippingsData' => $shippingsData
+            ]);
     }
 }
