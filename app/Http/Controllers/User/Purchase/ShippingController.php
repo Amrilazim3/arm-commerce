e3c25d383b4d876ef89f75ce253940d22271a6ba
Amrilazim3 <a.azim0711@gmail.com>
2022-11-17 14:27:20 +0800

    adding logic under productReceive method

--- app/Http/Controllers/User/Purchase/ShippingController.php
+++ app/Http/Controllers/User/Purchase/ShippingController.php
@@ -76,9 +76,9 @@ class ShippingController extends Controller
     {
         foreach ($request->orderIds as $id) {
             $ship = Shipping::where('order_id', $id)->first();
-            
+
             $ship->status = 'cancelled';
-            
+
             $ship->save();
         }
 
@@ -152,12 +152,75 @@ class ShippingController extends Controller
     {
         foreach ($request->orderIds as $id) {
             $ship = Shipping::where('order_id', $id)->first();
-            
+
             $ship->status = 'completed';
-            
+
             $ship->save();
         }
 
         return redirect()->back();
+    }
+
+    public function completed()
+    {
+        $shippingsData = Shipping::select([
+            'id',
+            'user_id',
+            'order_id',
+            'status',
+            'created_at'
+        ])
+            ->where('user_id', auth()->user()->id)
+            ->where('status', 'completed')
+            ->with(['order' => function ($item) {
+                return $item->select([
+                    'id',
+                    'cart_id',
+                    'bill_id',
+                    'address_id',
+                    'contact_email'
+                ])
+                    ->with(['address' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'full_name',
+                            'phone_number',
+                            'state',
+                            'city',
+                            'postal_code',
+                            'street_name'
+                        ]);
+                    }])
+                    ->with(['cart' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'product_id',
+                            'variant_name',
+                            'quantity',
+                            'price'
+                        ])
+                            ->with(['product' => function ($item) {
+                                return $item->select([
+                                    'id',
+                                    'name',
+                                    'slug'
+                                ])
+                                    ->with(['images' => function ($item) {
+                                        return $item->select([
+                                            'id',
+                                            'product_id',
+                                            'url'
+                                        ])->first();
+                                    }])
+                                    ->get();
+                            }])
+                            ->withTrashed();
+                    }]);
+            }])
+            ->get();
+
+        return Inertia::render('User/Purchase/Completed', [
+            'shippingsData' => $shippingsData
+        ]);
     }
 }
