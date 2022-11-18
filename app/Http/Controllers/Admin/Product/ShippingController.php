acae01a51c841576881a64a97d0e7e45b54d51d3
Amrilazim3 <a.azim0711@gmail.com>
2022-11-18 17:11:10 +0800

    after success changing status to shipped, send an email to customer

--- app/Http/Controllers/Admin/Product/ShippingController.php
+++ app/Http/Controllers/Admin/Product/ShippingController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Admin\Product;
+
 use App\Http\Controllers\Controller;
+use App\Jobs\TriggerOrderShippedEventJob;
+use App\Models\Order;
 use App\Models\Shipping;
 use Inertia\Inertia;
 
@@ -22,35 +25,36 @@ class ShippingController extends Controller
                     'address_id',
                     'contact_email'
                 ])
-                ->with(['address' => function ($item) {
-                    return $item->select([
-                        'id',
-                        'full_name',
-                        'phone_number',
-                        'state',
-                        'city',
-                        'postal_code',
-                        'street_name' 
-                    ]);
-                }])
-                ->with(['cart' => function ($item) {
-                    return $item->select([
-                        'id',
-                        'product_id',
-                        'variant_name',
-                        'quantity',
-                        'price'
-                    ])
-                        ->with(['product' => function ($item) {
-                            return $item->select([
-                                'id',
-                                'name',
-                                'slug'
-                            ])->get();
-                        }])
-                        ->withTrashed();
-                }]);
+                    ->with(['address' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'full_name',
+                            'phone_number',
+                            'state',
+                            'city',
+                            'postal_code',
+                            'street_name'
+                        ]);
+                    }])
+                    ->with(['cart' => function ($item) {
+                        return $item->select([
+                            'id',
+                            'product_id',
+                            'variant_name',
+                            'quantity',
+                            'price'
+                        ])
+                            ->with(['product' => function ($item) {
+                                return $item->select([
+                                    'id',
+                                    'name',
+                                    'slug'
+                                ])->get();
+                            }])
+                            ->withTrashed();
+                    }]);
             }])
+            ->orderBy('created_at', 'desc')
             ->paginate(10);
 
         return Inertia::render('Admin/Products/Shippings', [
@@ -63,8 +67,14 @@ class ShippingController extends Controller
         $shipping->status = 'shipped';
         
         $shipping->save();
+        
+        $order = Order::select([
+            'id',
+            'contact_email'
+        ])->where('id', $shipping->order_id)->first();
+
+        TriggerOrderShippedEventJob::dispatch($order->contact_email);
 
         return redirect()->back();
     }
 }
-
