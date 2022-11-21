c92c028e3c60d8c929e9168b5c6bf456b801a377
Amrilazim3 <a.azim0711@gmail.com>
2022-11-21 22:02:29 +0800

    get admin usr

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -18,8 +19,9 @@ class ProductFactory extends Factory
         foreach ($this->faker->words(4) as $name) {
             $productName .= $name . " ";
         }
+        $adminUser = User::where('email', 'a.azim0711@gmail.com')->first();
         return [
-            'user_id' => 11, // the only admin in this application
+            'user_id' => $adminUser->id, // the only admin in this application
             'category_id' => rand(1, 7), // only have seven category available,
             'name' => $productName,
             'slug' => Str::slug($productName),
