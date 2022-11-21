cb33b42b88b058da9ab0825c7813ea2833d4ff53
Amrilazim3 <a.azim0711@gmail.com>
2022-11-21 21:58:47 +0800

    comment all code

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,9 +14,9 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()->unverified()->make(); // create a user without persisting into database 
+        // $user = User::factory()->unverified()->make(); // create a user without persisting into database 
         // $user = User::factory()->count(3)->unverified()->create(); // create unverified email and persisting into database
         // $user = User::factory()->create(); // create default user and persisting into database
-        dd($user);
+        // dd($user);
     }
 }
