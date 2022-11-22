92530656a23a60763b144d474cc98348eb2175d2
Amrilazim3 <a.azim0711@gmail.com>
2022-11-22 22:04:13 +0800

    using laravel envoy to run multiple task

---
+++ Envoy.blade.php
@@ -0,0 +1,16 @@
+@servers(['web' => ['amril@128.199.131.110']])
+
+@story('deploy')
+    git
+    work
+@endstory
+
+@task('git')
+    cd /var/www/arm-commerce
+    git pull
+@endtask
+
+@task('work')
+    php artisan queue:work
+    php artisan schedule:run
+@endtask
